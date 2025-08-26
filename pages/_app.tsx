8cf64f7b20b41d2ac4be02d514e3ee8e7dbaaef2
D1stknight <miguel.a.concepcion@gmail.com>
2025-08-26 19:57:02 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
-import PrizeModalProvider from '../components/PrizeModalHost';
+import PrizeModalHost from '../components/PrizeModalHost';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PrizeModalProvider>
+    <>
       <Component {...pageProps} />
-    </PrizeModalProvider>
+      {/* Global prize modal listener/host */}
+      <PrizeModalHost />
+    </>
   );
 }
