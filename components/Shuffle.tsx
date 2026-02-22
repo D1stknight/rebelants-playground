08b25bdd7b4c130a3ce6186cfc54efb3dd5f3705
1stknight <miguel.a.concepcion@gmail.com>
2026-02-22 16:06:11 -0500

    Add profile loading and saving functions

--- components/Shuffle.tsx
+++ components/Shuffle.tsx
@@ -5,6 +5,9 @@ import Link from 'next/link'; // <-- NEW: real navigation for header tabs
 import { shuffleConfig } from '../lib/shuffleConfig';
 import { pointsConfig } from "../lib/pointsConfig";
 import { usePoints } from "../lib/usePoints";
+import { loadProfile, saveProfile } from "../lib/profile";
+import { addWin } from "../lib/winsStore";
+import LeaderboardPanel from "./LeaderboardPanel";
 
 // lazy‑load queen so 3D never blocks SSR
 const Queen3D = dynamic(() => import('./Queen3D'), { ssr: false }) as React.ComponentType<{
