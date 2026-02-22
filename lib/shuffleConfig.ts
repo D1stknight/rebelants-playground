46b8946c63bda318bd3c024e1d59f7378a9af2e4
1stknight <miguel.a.concepcion@gmail.com>
2026-02-22 11:56:22 -0500

    Add shuffleConfig for game configuration settings

---
+++ lib/shuffleConfig.ts
@@ -0,0 +1,16 @@
+// lib/shuffleConfig.ts
+
+export const shuffleConfig = {
+  // 🔢 Egg count (we're doing 5 like you wanted)
+  eggCount: 5,
+
+  // 🎨 Backgrounds
+  pageBg: "/bg/colony.png",
+  cardBg: "/bg/dojo.png",
+
+  // 👑 Queen model
+  queenModel: "/models/queen/queen.glb",
+
+  // 👑 Final size tweak
+  queenScale: 0.7,
+};
