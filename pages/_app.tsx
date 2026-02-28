f00d5b3d02941227dfefbae1805020356dee12fe
1stknight <miguel.a.concepcion@gmail.com>
2026-02-27 20:33:23 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,32 @@
 // pages/_app.tsx
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
-import PrizeModalProvider from '../components/PrizeModalHost';
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+import type { AppProps } from "next/app";
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { apeChain } from "../lib/apechain";
+
+const queryClient = new QueryClient();
+
+const wagmiConfig = getDefaultConfig({
+  appName: "Rebel Ants Playground",
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "MISSING_PROJECT_ID",
+  chains: [apeChain],
+  transports: {
+    [apeChain.id]: http(),
+  },
+});
+
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PrizeModalProvider>
-      <Component {...pageProps} />
-    </PrizeModalProvider>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
