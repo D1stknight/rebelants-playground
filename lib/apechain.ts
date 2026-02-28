25613290bd8ef03a2ab13ed1fa047dcbfa5c9ffd
1stknight <miguel.a.concepcion@gmail.com>
2026-02-27 20:28:19 -0500

    Create apechain.ts

---
+++ lib/apechain.ts
@@ -0,0 +1,20 @@
+// lib/apechain.ts
+import type { Chain } from "viem";
+
+export const apeChain: Chain = {
+  id: 33139,
+  name: "ApeChain",
+  network: "apechain",
+  nativeCurrency: {
+    name: "ApeCoin",
+    symbol: "APE",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: { http: ["https://apechain.calderachain.xyz/http"] },
+    public: { http: ["https://apechain.calderachain.xyz/http"] },
+  },
+  blockExplorers: {
+    default: { name: "ApeChain Explorer", url: "https://apechain.calderaexplorer.xyz" },
+  },
+};
