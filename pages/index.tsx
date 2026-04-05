e81fd2ff4650afa7bb1f4542c148a5ac966dcd3a
1stknight <miguel.a.concepcion@gmail.com>
2026-04-05 17:06:16 -0400

    fix: homepage redirects to /hatch (Faction Wars)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,6 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 export default function Home() {
   const router = useRouter();
-  useEffect(() => { router.replace("/expedition"); }, []);
+  useEffect(() => { router.replace("/hatch"); }, []);
   return null;
 }
