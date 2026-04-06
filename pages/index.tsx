cdbaeaf3dc9b594e6566e87b4dbbd832e8d7f786
1stknight <miguel.a.concepcion@gmail.com>
2026-04-05 20:08:24 -0400

    fix: redirect / to /faction-wars

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,6 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 export default function Home() {
   const router = useRouter();
-  useEffect(() => { router.replace("/hatch"); }, []);
+  useEffect(() => { router.replace("/faction-wars"); }, []);
   return null;
 }
