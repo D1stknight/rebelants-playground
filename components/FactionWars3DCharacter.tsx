60b6864c80ca9933514f36cf00495b297d4a608c
1stknight <miguel.a.concepcion@gmail.com>
2026-05-01 22:11:59 -0400

    Update FactionWars3DCharacter.tsx

--- components/FactionWars3DCharacter.tsx
+++ components/FactionWars3DCharacter.tsx
@@ -52,7 +52,7 @@ function SamuraiModel({ side = "player" }: { side?: "player" | "enemy" }) {
 
     // Your GLB has strange huge bounds, so for now we use manual placement.
     // We are intentionally NOT doing scene.position.sub(center) in this test.
-         scene.position.set(0, -0.30, 0);
+         scene.position.set(0, -0.25, 0);
     scene.scale.setScalar(0.020);
 
     return scene;
